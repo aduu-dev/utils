2592a12c418abf3d32d04bd5e9abcd5612f9dea4
fabstu <fabian.sturm@aduu.de>
2020-11-06 12:43:43 +0100

    added aduu.dev/utils/logtesting: setting klog logLevel inside test to see klog debug messages

---
+++ logtesting/klog.go
@@ -0,0 +1,17 @@
+package logtesting
+
+import (
+	"flag"
+
+	"k8s.io/klog/v2"
+)
+
+// LogLevel5 sets the log level to 5 for testing.
+func LogLevel5() {
+	fs := flag.NewFlagSet("log", flag.ExitOnError)
+	klog.InitFlags(fs)
+
+	if err := fs.Parse([]string{"-v", "5"}); err != nil {
+		panic("failed to parse flags")
+	}
+}
---
+++ logtesting/klog_test.go
@@ -0,0 +1,56 @@
+package logtesting
+
+import (
+	"flag"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"aduu.dev/utils/go/gomod"
+	"aduu.dev/utils/tempdir"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/klog/v2"
+)
+
+func TestLogLevel5(t *testing.T) {
+	dir := tempdir.MakeTempDir(gomod.DirectCallerFunctionNameWithPackageName())
+	t.Cleanup(func() {
+		tempdir.DeleteTempDir(dir)
+	})
+	tempFile := filepath.Join(dir, "logs.log")
+
+	LogLevel5()
+
+	// The the log-file to read from.
+	fs := flag.NewFlagSet("log_testing", flag.ExitOnError)
+	klog.InitFlags(fs)
+
+	fs.Set("logtostderr", "false")
+	fs.Set("log_file", tempFile)
+
+	if err := fs.Parse([]string{}); err != nil {
+		panic("failed to parse flags")
+	}
+
+	klog.V(5).InfoS("hello world")
+	klog.InfoS("hey du")
+
+	// Make sure the log message is written.
+	klog.Flush()
+
+	got, err := ioutil.ReadFile(tempFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gotString := string(got)
+	wantString := "hello world"
+
+	if !assert.True(t,
+		strings.Contains(gotString, wantString),
+		"log should contain %#v, but is %#v", wantString, gotString,
+	) {
+		t.Fail()
+	}
+}
